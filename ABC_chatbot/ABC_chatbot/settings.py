ddcbc61eba6060d70438c89972f3fa836c7a772c
AnhDinh <58550812+AnhDinhJan26@users.noreply.github.com>
2022-03-29 19:02:34 +0700

    2

--- ABC_chatbot/ABC_chatbot/settings.py
+++ ABC_chatbot/ABC_chatbot/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_HOST = os.environ.get('DJANGO_STATIC_HOST', '')
 STATIC_URL = STATIC_HOST + '/static/'
